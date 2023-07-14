4a20920e627cdf67b326a0ae0821b7f9d9732b1a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-07-14 18:30:28 -0300

    feat: calculo da media de tempo de uso por categoria de usuario (casual ou membro) com geraçao de dataviz

---
+++ mean_riding_time_per_category.r
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(tidyverse)
+
+# Definição do Workdir (onde estão os csvs analisados)
+setwd("~/Git/data_analysis_case_study_cyclistic/csv/")
+
+# Carregamento dos csvs
+bikes_csv_files <-
+  list.files(path = ".", pattern = "*.csv", recursive = FALSE) %>% 
+  map_df(~read_csv(.))
+
+# Calcular tempo médio de uso dos membros vs. usuários casuais
+bikes_mean_riding_time_data <- bikes_csv_files %>% 
+  drop_na() %>% 
+  mutate(bikes_csv_files, riding_time=difftime(ended_at, started_at, units = "mins")) %>% 
+  group_by(member_casual) %>% 
+  summarise(mean_riding_time = round(mean(riding_time), digits = 1))
+
+# Geração do gráfico do tempo médio de uso por categoria
+ggplot(data = bikes_mean_riding_time_data,
+       mapping = aes(
+         x = mean_riding_time,
+         y = member_casual,
+         fill = member_casual
+       )) + ggtitle("Tempo médio de uso por categoria de usuário") +
+         theme(plot.title = element_text(hjust = 0.3)) +
+  geom_col() +
+  labs(
+    x = "Tempo médio de uso",
+    y = "Categoria de usuário"
+  ) +
+  guides(fill = "none")
+  
