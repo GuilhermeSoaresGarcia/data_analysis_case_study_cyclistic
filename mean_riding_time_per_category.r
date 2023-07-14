e4ee4baec05e40ad63ebf0eee6db8b3a8e9e38e1
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-07-14 20:16:59 -0300

    feat: implementa funçao para gerar subtitulo ao dataviz que informa a quantidade de registros utilizados para a analise de dados bem como data inicial e final desses registros

--- mean_riding_time_per_category.r
+++ mean_riding_time_per_category.r
@@ -1,5 +1,5 @@
-library(ggplot2)
 library(tidyverse)
+library(ggplot2)
 
 # Definição do Workdir (onde estão os csvs analisados)
 setwd("~/Git/data_analysis_case_study_cyclistic/csv/")
@@ -9,6 +9,11 @@ bikes_csv_files <-
   list.files(path = ".", pattern = "*.csv", recursive = FALSE) %>% 
   map_df(~read_csv(.)) %>% 
   drop_na()
+
+# Informações sobre os registros (utilizadas no subtítulo do gráfico)
+total_registries <- count(bikes_csv_files)
+newest_registry <- format(as.Date(max(bikes_csv_files$started_at), format="%Y/%m/%d"), "%d/%m/%Y")
+oldest_registry <- format(as.Date(min(bikes_csv_files$started_at), format="%Y/%m/%d"), "%d/%m/%Y")
 
 # Calcular tempo médio de uso dos membros vs. usuários casuais
 bikes_mean_riding_time_data <- bikes_csv_files %>% 
@@ -22,12 +27,16 @@ ggplot(data = bikes_mean_riding_time_data,
          x = mean_riding_time,
          y = member_casual,
          fill = member_casual
-       )) + ggtitle("Tempo médio de uso por categoria de usuário") +
-         theme(plot.title = element_text(hjust = 0.3)) +
+       )) + 
   geom_col() +
   labs(
+    title = "Tempo médio de uso por categoria de usuário",
+    subtitle = paste("Baseado em", total_registries, "registros coletados entre", oldest_registry, "e", newest_registry),
     x = "Tempo médio de uso (em minutos)",
     y = "Categoria de usuário"
   ) +
+  theme(
+    plot.title = element_text(hjust = 0.5), 
+    plot.subtitle = element_text(hjust = 0.5)
+    ) +
   guides(fill = "none")
-  
